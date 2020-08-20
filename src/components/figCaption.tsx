e15d5787213e8bba95f0bbf463b68af400ea5178
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2020-08-20 14:00:48 +0100

    BodyImage Component (#10)
    - Added an `editorialPalette` module for SVG colours
    - Added a BodyImage component and stories
    - Added a FigCaption component and stories
    - Pulled the image used for stories into a fixtures file
    - Better names for palette colours
    - Apply suggestions from code review
    
    Co-authored-by: Max Duval <max.duval@guardian.co.uk>

---
+++ src/components/figCaption.tsx
@@ -0,0 +1,97 @@
+/** @jsx jsx */
+// ----- Imports ----- //
+
+import { FC, ReactNode } from 'react';
+import { Format, Design } from '@guardian/types/Format';
+import { Option, OptionKind } from '@guardian/types/option';
+import { remSpace } from '@guardian/src-foundations';
+import { text, neutral } from '@guardian/src-foundations/palette';
+import { textSans } from '@guardian/src-foundations/typography';
+import { SerializedStyles, css, jsx } from '@emotion/core';
+
+import { darkModeCss } from 'lib';
+import { fill } from 'editorialPalette';
+
+
+// ----- Sub-Components ----- //
+
+interface TriangleProps {
+    format: Format;
+    supportsDarkMode: boolean;
+}
+
+const triangleStyles = (format: Format, supportsDarkMode: boolean): SerializedStyles => css`
+    fill: ${fill.iconPrimary(format)};
+    height: 0.8em;
+    padding-right: ${remSpace[1]};
+
+    ${darkModeCss(supportsDarkMode)`
+        fill: ${fill.iconPrimaryInverse(format)};
+    `}
+`;
+
+const Triangle: FC<TriangleProps> = ({ format, supportsDarkMode }) => {
+    switch (format.design) {
+        case Design.Media:
+            return null;
+        default:
+            return (
+                <svg
+                    css={triangleStyles(format, supportsDarkMode)}
+                    viewBox="0 0 10 9"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <polygon points="0,9 5,0 10,9" />
+                </svg>
+            );
+    }
+}
+
+
+// ----- Component ----- //
+
+interface Props {
+    format: Format;
+    supportsDarkMode: boolean;
+    children: Option<ReactNode>;
+}
+
+const styles = css`
+    ${textSans.xsmall()}
+    padding-top: ${remSpace[2]};
+    color: ${text.supporting};
+`;
+
+const mediaStyles = css`
+    color: ${neutral[86]};
+`;
+
+const getStyles = (format: Format): SerializedStyles => {
+    switch (format.design) {
+        case Design.Media:
+            return css(styles, mediaStyles);
+        default:
+            return styles;
+    }
+}
+
+const FigCaption: FC<Props> = ({ format, supportsDarkMode, children }) => {
+    switch (children.kind) {
+        case OptionKind.Some:
+            return (
+                <figcaption css={getStyles(format)}>
+                    <Triangle format={format} supportsDarkMode={supportsDarkMode} />
+                    {children.value}
+                </figcaption>
+            );
+
+        default:
+            return null;
+    }
+    
+}
+
+
+// ----- Exports ----- //
+
+export default FigCaption;
