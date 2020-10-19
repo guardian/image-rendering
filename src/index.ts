a0ba70ebcac379c85041e0a7581ce7ddf54dfbb1
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2020-10-19 19:41:00 +0100

    Index File (#74)
    - Created index file for cleaner imports
    - Version 1.1.0

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// ----- Types ----- //
+
+export type { Image } from './image';
+export { Role } from './image';
+export type { Lightbox } from './lightbox';
+export type { Sizes } from './sizes';
+
+
+// ----- Components ----- //
+
+export { default as BodyImage } from './components/bodyImage';
+export { default as FigCaption } from './components/figCaption';
+export { default as Img } from './components/img';
