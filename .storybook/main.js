3a10afb048fed67cb691d41d8da98efbe28fd706
Alex Sanders <a@snd.rs>
2021-09-06 16:20:17 +0100

    include `@guardian` modules in storybook transpilation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,20 @@
 const path = require('path');
 
+const nodeModulesExclude = [
+    {
+        test: /node_modules/,
+        exclude: [
+            /@guardian\//,
+        ],
+    },
+]
+
 module.exports = {
     stories: ['../src/**/*.stories.tsx'],
     webpackFinal: async config => {
         config.module.rules.push({
             test: /\.(ts|tsx)$/,
+            exclude: nodeModulesExclude,
             use: [
                 {
                     loader: require.resolve('babel-loader'),
@@ -21,6 +31,11 @@ module.exports = {
                 },
             ],
         });
+
+        // update storybook webpack config to transpile *all* JS
+        config.module.rules.find(rule => String(rule.test) === String(/\.(mjs|tsx?|jsx?)$/))
+            .exclude = nodeModulesExclude;
+
         config.resolve.extensions.push('.ts', '.tsx');
         config.resolve.modules.push(path.resolve(__dirname, '../src'));
 
